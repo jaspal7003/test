87228038e0c17c66c4e6de81dac236edf4cca9fc
jaspal7003 <99255155+jaspal7003@users.noreply.github.com>
2022-10-21 13:51:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/get', (req, res) => {
 app.get('/set/:key/:value', (req, res) => {
     const {key, value} = req.params;
     try{
-        res.cookie(key, value, {sameSite:"none", strict: true});
+        res.cookie(key, value, {sameSite:"lex"});
     }catch(err) {
         res.json(err);
     }
